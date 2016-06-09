3c166e02087492265e8c7828f7d273c7bf74bd19
Bernardo Ferreira <bernardolferreira@gmail.com>
2016-06-09 18:20:31 +0100

    Updated copyright notices.

--- projects/cloud/src/main/java/pt/unlfctdi/cryptosearch/cloud/search/PrototypeSearchBean.java
+++ projects/cloud/src/main/java/pt/unlfctdi/cryptosearch/cloud/search/PrototypeSearchBean.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2013 Bernardo Ferreira
+ *    Copyright 2013 Bernardo Luís da Silva Ferreira
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
